569ebba8d20a544ec1541a9c10223e942e21ae6a
tyduptyler13 <tyduptyler13@yahoo.com>
2014-04-28 16:29:58 -0600

    Revamped the WordCounter.

--- src/com/myuplay/CS455/Hadoop/WordMapper.java
+++ src/com/myuplay/CS455/Hadoop/WordMapper.java
@@ -1,22 +1,19 @@
 package com.myuplay.CS455.Hadoop;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 
 public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
 
-	private Text word = new Text();
 	private final static IntWritable one = new IntWritable(1);
 
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
 
-		StringTokenizer wordList = new StringTokenizer(value.toString());
-		while (wordList.hasMoreTokens()){
-			word.set(wordList.nextToken());
-			context.write(word, one);
+		String input = value.toString();
+		String[] words = input.toLowerCase().replaceAll("[^a-z0-9-]", "").split("\\s+");
+		for (String word : words){
+			context.write(new Text(word), one);
 		}
 
 	}
