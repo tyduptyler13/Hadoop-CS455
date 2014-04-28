db53f0942c80c8e1ee409c796cbe06398f831102
tyduptyler13 <tyduptyler13@yahoo.com>
2014-04-28 16:45:50 -0600

    Improved and fixed problems in wordcount

--- src/com/myuplay/CS455/Hadoop/WordMapper.java
+++ src/com/myuplay/CS455/Hadoop/WordMapper.java
@@ -1,17 +1,20 @@
 package com.myuplay.CS455.Hadoop;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
+
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 
 public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
 
 	private final static IntWritable one = new IntWritable(1);
+	private final static Pattern pattern = Pattern.compile("[^a-zA-Z\\d\\s:]");
 
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
 
-		String input = value.toString();
-		String[] words = input.toLowerCase().replaceAll("[^a-z0-9-]", "").split("\\s+");
+		String input = value.toString().toLowerCase();
+		String[] words = pattern.matcher(input).replaceAll("").split("\\s+");//Precompiled regex.
 		for (String word : words){
 			context.write(new Text(word), one);
 		}
