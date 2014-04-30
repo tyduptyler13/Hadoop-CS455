93f51336510a39d764d7bf9f5db98332968b5f0b
tyduptyler13 <tyduptyler13@yahoo.com>
2014-04-30 15:04:04 -0600

    Revamped to clean out weird cases.

--- src/cs455/hadoop/NGramMapper.java
+++ src/cs455/hadoop/NGramMapper.java
@@ -1,6 +1,8 @@
 package cs455.hadoop;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -20,7 +22,17 @@ public class NGramMapper extends Mapper<Object, Text, Text, IntWritable> {
 		String file = ((FileSplit) context.getInputSplit()).getPath().getName();
 
 		String input = value.toString().toUpperCase(Locale.ENGLISH);
-		String[] words = split.split(input);
+		String[] unfiltered = split.split(input);
+
+		List<String> words = new ArrayList<String>();
+		//Filter once
+		for (String word : unfiltered){
+			if (word.length() > 0){
+				words.add(word.trim()); //Ensured to be clean now.
+			}
+		}
+
+		//The following could be improved for better memory access.
 
 		//N-Gram(1)
 		for (String word : words){
@@ -29,14 +41,14 @@ public class NGramMapper extends Mapper<Object, Text, Text, IntWritable> {
 		}
 
 		//N-Gram(2)
-		for (int i = 0; i < words.length - 1; ++i){
-			t.set(file + " " + words[i] + " " + words[i+1]);
+		for (int i = 0; i < words.size() - 1; ++i){
+			t.set(file + " " + words.get(i) + " " + words.get(i+1));
 			context.write(t, one);
 		}
 
 		//N-Gram(3)
-		for (int i = 0; i < words.length - 2; ++i){
-			t.set(file + " " + words[i] + " " + words[i+1] + " " + words[i+2]);
+		for (int i = 0; i < words.size() - 2; ++i){
+			t.set(file + " " + words.get(i) + " " + words.get(i+1) + " " + words.get(i+2));
 			context.write(t, one);
 		}
 
