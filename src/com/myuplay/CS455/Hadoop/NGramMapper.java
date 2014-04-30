52c8cee20154ff50b13e989bfd66cd2d895e64b5
tyduptyler13 <tyduptyler13@yahoo.com>
2014-04-29 21:37:35 -0600

    Upgraded to an NGramMapper

---
+++ src/com/myuplay/CS455/Hadoop/NGramMapper.java
@@ -0,0 +1,48 @@
+package com.myuplay.CS455.Hadoop;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+
+public class NGramMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+	private static final IntWritable one = new IntWritable(1);
+	private static final Pattern replace = Pattern.compile("[\\n]+");
+	private static final Pattern remove = Pattern.compile("[^a-zA-Z\\d :-\\$]");
+	private static final Pattern split = Pattern.compile("[\\s\t]+");
+
+	private static final String empty = "";
+	private static final String space = " ";
+	
+	private static Text t = new Text();
+	
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+
+		String input = value.toString().toLowerCase();
+		input = replace.matcher(input).replaceAll(space);
+		input = remove.matcher(input).replaceAll(empty);//Precompiled regex.
+		String[] words = split.split(input);
+		
+		//N-Gram(1)
+		for (String word : words){
+			t.set(word);
+			context.write(t, one);
+		}
+		
+		//N-Gram(2)
+		for (int i = 0; i < words.length - 1; ++i){
+			t.set(words[i] + " " + words[i+1]);
+			context.write(t, one);
+		}
+		
+		//N-Gram(3)
+		for (int i = 0; i < words.length - 2; ++i){
+			t.set(words[i] + " " + words[i+1] + " " + words[i+2]);
+			context.write(t, one);
+		}
+
+	}
+
+}
